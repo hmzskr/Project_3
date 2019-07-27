fc4b55c2b0b3fd39b98a69ed69e118480e9f005b
Marcus McBride <marcusmcb@gmail.com>
2019-07-27 11:15:11 -0700

    added basic flask app

---
+++ front_end/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+     return render_template('index.html')
+
+@app.route('/api/data')
+def data():
+     return jsonify()
+
+@app.route('/cityInput', methods=['POST'])
+def cityInput():
+     city_input = request.form['city_input']
+     return ()
+     
+@app.route('/zip_and_category', methods=['POST'])
+def zip_and_category():
+     zip_code = request.form['zip_code']
+     category_input = request.form['category_input']
+     return ()
+
+
+if __name__ == "__main__":
+     app.run(debug=True, use_reloader=False)
